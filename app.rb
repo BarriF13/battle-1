f9d86c06da1a2b70de361a89356161d15164513f
Mrtly <mrtly.ca@gmail.com>
2020-02-04 15:56:25 +0000

    we can enter our names and see them

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require "sinatra/base"
 
-class MyApp < Sinatra::Base
-  get "/" do
-    "Hello Battle!"
-  end
+class Battle < Sinatra::Base
+
+get "/" do
+  erb :index
+end
+
+post '/names' do
+  @player_1_name = params[:player_1_name]
+  @player_2_name = params[:player_2_name]
+  erb :play
+end
+
 end
