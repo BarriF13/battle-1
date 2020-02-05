2334c86dd83e3ade77aa71131e4e2767eac543c4
Mrtly <mrtly.ca@gmail.com>
2020-02-05 15:26:25 +0000

    added post-redirect loop

--- Gemfile
+++ Gemfile
@@ -1,7 +1,4 @@
 source "https://rubygems.org"
 gem "sinatra"
 gem "rspec"
-<<<<<<< HEAD
-=======
 gem "capybara"
->>>>>>> f9d86c06da1a2b70de361a89356161d15164513f
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,8 +2,8 @@ feature 'Enter names' do
     scenario 'submitting names' do
       visit('/')
       fill_in :player_1_name, with: 'Mrtl'
-      fill_in :player_2_name, with: 'Bari'
+      fill_in :player_2_name, with: 'Hay'
       click_button 'Submit'
-      expect(page).to have_content 'Mrtl vs. Bari'
+      expect(page).to have_content 'Mrtl vs. Hay'
     end
     end
