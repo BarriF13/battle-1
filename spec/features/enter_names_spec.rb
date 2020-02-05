776ecea106744bfe3d6b0a5e0851453eb25c8036
Mrtly <mrtly.ca@gmail.com>
2020-02-05 15:38:36 +0000

    can see player 2 points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,4 +6,14 @@ feature 'Enter names' do
       click_button 'Submit'
       expect(page).to have_content 'Mrtl vs. Hay'
     end
+  end
+
+feature 'Player1 can see Player2 Hit Points' do
+  scenario 'can display hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Mrtl'
+      fill_in :player_2_name, with: 'Hay'
+      click_button 'Submit'
+      expect(page).to have_content 'opponent points: 0'
     end
+end
